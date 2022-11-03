c5f71ef9066410d10b39eae456d42761ced22b2c
keplib <keplib@gmail.com>
2022-11-03 18:58:48 +0100

    Exercise 1.3 solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,40 @@ import Total from './components/Total';
 
 
 const App = () => {
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
 
   return (
     <div>
       <Header header={course}/>
       <Content 
-        part1={part1} point1={exercises1}
-        part2={part2} point2={exercises2}
-        part3={part3} point3={exercises3}
+        part1={part1.name} point1={part1.exercises}
+        part2={part2.name} point2={part2.exercises}
+        part3={part3.name} point3={part3.exercises}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={part1.exercises} 
+             exercises2={part2.exercises} 
+             exercises3={part3.exercises}/>
     </div>
   )
 }
