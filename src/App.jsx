66c4a9d712807e13f5f53bcfc6e716e49b4beadc
keplib <keplib@gmail.com>
2022-11-03 19:24:35 +0100

    Exercise 1.5 solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,48 @@ import Total from './components/Total';
 
 const App = () => {
 
-  const course = 'Half Stack application development'
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  // const course = 'Half Stack application development'
+
+  // const parts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name: 'State of a component',
+  //     exercises: 14
+  //   }
+  // ]
 
 
   return (
     <div>
-      <Header header={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header header={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
